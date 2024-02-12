bb4c6a4baf1a533ab5d7586402a0c9b2b061d7e8
Lucky Pradana <lucky@ampersanda.dev>
2024-02-12 15:19:28 +0700

    feat: first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Requirements
+- [Playdate SDK](https://play.date/dev/)
+
+## Build and Run
+```shell
+mkdir -p bin/Output.pdx; pdc -k source bin/Notee.pdx; open bin/Notee.pdx
+```
---
+++ source/main.lua
@@ -0,0 +1,48 @@
+import "CoreLibs/graphics"
+import "CoreLibs/keyboard"
+
+local gfx <const> = playdate.graphics
+local kbd <const> = playdate.keyboard
+
+initial = "Type note..."
+keyboard = ""
+
+function setup()
+    keyboard = playdate.datastore.read() or ""
+end
+
+setup()
+
+function playdate.update()
+    gfx.clear()
+
+    if kbd.isVisible() ~= true and playdate.buttonIsPressed(playdate.kButtonA) then
+        kbd.show(keyboard)
+    end
+
+    if keyboard == "" then
+        gfx.drawText(initial, 16, 16)
+        kbd.show(keyboard)
+    else
+        gfx.drawText(keyboard, 16, 16)
+    end
+
+    gfx.drawTextAligned("*Press* Ⓐ *to edit*", playdate.display.getWidth() - 16, playdate.display.getHeight() - 32, kTextAlignment.right)
+end
+
+function kbd.textChangedCallback()
+    keyboard = kbd.text
+end
+
+function kbd.keyboardWillHideCallback(ok)
+    if ok then
+        playdate.datastore.write(keyboard)
+    else
+        displayText = playdate.datastore.read() or ""
+    end
+end
+
+function playdate.gameWillSleep()
+    playdate.datastore.write(keyboard)
+end
+
